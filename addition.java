bfd021737854ff2ff4182d356203813327f1eec2
HarsH-ka242 <harshaggarwl05@gmail.com>
2024-11-13 14:34:53 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main( String[]args){
+        int a =3;
+        int b=5;
+        int c=7;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
